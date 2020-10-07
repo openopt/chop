Bug fixes and broadcasting

constopt/constraints.py
@@ -98,12 +98,13 @@ class LpBall:
 
     def fw_gap(self, grad, iterate):
         update_direction, _ = self.lmo(-grad, iterate)
-        return -grad.dot(update_direction)
+        return (-grad * update_direction).sum()
 
 
 class LinfBall(LpBall):
     def __init__(self, alpha):
         super(LinfBall, self).__init__(alpha)
+        self.p = np.inf
 
     def prox(self, x, step_size=None):
         if torch.max(abs(x)) <= self.alpha:
@@ -120,6 +121,7 @@ class L1Ball(LpBall):
 
     def __init__(self, alpha):
         super(L1Ball, self).__init__(alpha)
+        self.p = 1
 
     def lmo(self, grad, iterate):
         """Returns s-x, x solving the linear problem
@@ -127,7 +129,7 @@ class L1Ball(LpBall):
         """
         update_direction = -iterate.clone().detach()
         abs_grad = abs(grad)
-        largest_coordinate = torch.argmax(abs_grad)
+        largest_coordinate = torch.where(abs_grad == abs_grad.max())
 
         update_direction[largest_coordinate] += self.alpha * torch.sign(
             grad[largest_coordinate])
@@ -140,6 +142,7 @@ class L1Ball(LpBall):
 class L2Ball(LpBall):
     def __init__(self, alpha):
         super(L2Ball, self).__init__(alpha)
+        self.p = 2
 
     def prox(self, x, step_size=None):
         norm = torch.norm(x)

constopt/optim.py
@@ -7,9 +7,10 @@ https://github.com/openopt/copt"""
 class PGD(Optimizer):
     """Projected Gradient Descent"""
 
-    def __init__(self, params, prox):
-        self.prox = prox
-        defaults = dict(prox=prox)
+    def __init__(self, params, constraint):
+        self.prox = constraint.prox
+        self.name = 'PGD'
+        defaults = dict(prox=self.prox, name=self.name)
         super(PGD, self).__init__(params, defaults)
 
     @torch.no_grad()
@@ -39,10 +40,11 @@ class PGD(Optimizer):
 class PGDMadry(Optimizer):
     """What Madry et al. call PGD"""
 
-    def __init__(self, params, prox, lmo):
-        self.prox = prox
-        self.lmo = lmo
-        defaults = dict(prox=prox, lmo=lmo)
+    def __init__(self, params, constraint):
+        self.prox = constraint.prox
+        self.lmo = constraint.lmo
+        self.name = 'PGD-Madry'
+        defaults = dict(prox=self.prox, lmo=self.lmo, name=self.name)
         super(PGDMadry, self).__init__(params, defaults)
 
     @torch.no_grad()
@@ -74,9 +76,10 @@ class PGDMadry(Optimizer):
 class FrankWolfe(Optimizer):
     """Vanilla Frank-Wolfe algorithm"""
 
-    def __init__(self, params, lmo):
-        self.lmo = lmo
-        defaults = dict(lmo=lmo)
+    def __init__(self, params, constraint):
+        self.lmo = constraint.lmo
+        self.name = 'Vanilla-FW'
+        defaults = dict(lmo=self.lmo, name=self.name)
         super(FrankWolfe, self).__init__(params, defaults)
 
     @torch.no_grad()
@@ -107,15 +110,17 @@ class FrankWolfe(Optimizer):
 
                 update_direction, _ = self.lmo(-p.grad, p)
                 p += (2. / (state['step'] + 2.)) * update_direction
+                print((p ** 2).sum())
         return loss
 
 
 class MomentumFrankWolfe(Optimizer):
     """Class for the Stochastic Frank-Wolfe algorithm given in Mokhtari et al"""
 
-    def __init__(self, params, lmo):
-        self.lmo = lmo
-        defaults = dict(lmo=lmo)
+    def __init__(self, params, constraint):
+        self.lmo = constraint.lmo
+        self.name = 'Momentum-FW'
+        defaults = dict(lmo=self.lmo, name=self.name)
         super(MomentumFrankWolfe, self).__init__(params, defaults)
 
 
@@ -144,7 +149,7 @@ class MomentumFrankWolfe(Optimizer):
                     state['grad_estimate'] = torch.zeros_like(
                         p, memory_format=torch.preserve_format)
 
-                state['step'] += 1
+                state['step'] += 1.
                 state['grad_estimate'] += ((1. / (state['step'] + 1)) ** (1/3)
                                            * (grad - state['grad_estimate']))
                 update_direction, _ = self.lmo(-state['grad_estimate'], p)

tests/test_optim.py
@@ -4,11 +4,14 @@ import numpy as np
 import torch
 from torch.autograd import Variable
 import pytest
-import constopt
 
 from cox.store import Store
 
-OUT_DIR = "logging/tests/"
+import constopt
+from constopt import optim
+
+
+OUT_DIR = "logging/tests/test_optim"
 MAX_ITER = 300
 
 torch.manual_seed(0)
@@ -26,44 +29,37 @@ y = abs(y / y.max())
 tol = 4e-3
 
 
-@pytest.mark.parametrize('algorithm', ['vanilla-FW', 'momentum-FW', 'PGD', 'PGD-Madry'])
+@pytest.mark.parametrize('algorithm', [optim.PGD, optim.PGDMadry,
+                                       optim.FrankWolfe, optim.MomentumFrankWolfe])
 @pytest.mark.parametrize('step_size', [.1, .05, .001, 0.])
 def test_L1Ball(algorithm, step_size):
-    store = Store(OUT_DIR)
-    store.add_table('metadata', {'algorithm': str, 'step-size': float})
-
-    store['metadata'].append_row({'algorithm': algorithm, 'step-size': step_size})
-    store.add_table(algorithm, {'func_val': float, 'FW gap': float,
-                                'norm(w_t)': float})
-
+    # Setup
     constraint = constopt.constraints.L1Ball(alpha)
     assert (constraint.prox(w) == w).all()
     w_t = Variable(torch.zeros_like(w), requires_grad=True)
 
-    if algorithm == "vanilla-FW":
-        optimizer = constopt.optim.FrankWolfe([w_t], constraint.lmo)
-    elif algorithm == 'momentum-FW':
-        optimizer = constopt.optim.MomentumFrankWolfe([w_t], constraint.lmo)
-    elif algorithm == 'PGD':
-        optimizer = constopt.optim.PGD([w_t], constraint.prox)
-    elif algorithm == 'PGD-Madry':
-        optimizer = constopt.optim.PGDMadry([w_t], constraint.prox,
-                                            constraint.lmo)
+    optimizer = algorithm([w_t], constraint)
     criterion = torch.nn.MSELoss(reduction='mean')
+
+    # Logging
+    store = Store(OUT_DIR)
+    store.add_table('metadata', {'algorithm': str, 'step-size': float})
+
+    store['metadata'].append_row({'algorithm': optimizer.name, 'step-size': step_size})
+    store.add_table(optimizer.name, {'func_val': float, 'FW gap': float,
+                                     'norm(w_t)': float})
     loss = criterion(X.mv(w_t), y)
-    ii = 0
     gap = torch.tensor(np.inf)
     for ii in range(MAX_ITER):
-        store.log_table_and_tb(algorithm, {'func_val': loss.item(),
-                                           'FW gap': gap.item(),
-                                           'norm(w_t)': sum(abs(w_t)).item()
-                                           })
-        store[algorithm].flush_row()
+        store.log_table_and_tb(optimizer.name, {'func_val': loss.item(),
+                                                'FW gap': gap.item(),
+                                                'norm(w_t)': sum(abs(w_t)).item()
+                                                })
+        store[optimizer.name].flush_row()
         loss.backward()
         with torch.no_grad():
             gap = constraint.fw_gap(w_t.grad, w_t)
         optimizer.step(step_size)
         loss = criterion(X.mv(w_t), y)
 
-        ii += 1
     store.close()