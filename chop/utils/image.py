Utils refactor ; Fixed ImageNet Example ; Added normalization + data loading utils

chop/utils/image.py
@@ -22,30 +22,30 @@ def matplotlib_imshow(img, one_channel=False, ax=None):
 
 
 def matplotlib_imshow_batch(batch, labels=None, one_channel=False, axes=None, normalize=False, range=(0., 1.),
-                            title=""):
+                            title="", negative=False):
     npimgs = [img.detach().cpu().numpy() for img in batch]
     if labels is None:
         labels = [""] * batch.size(0)
-    axes[0].set_ylabel(title)
+    axes[0].set_title(title)
     for ax, img, label in zip(axes, npimgs, labels):
         if one_channel:
             if normalize:
-                img = normalize_image(img, range, True)
+                img = normalize_image(img, range, True, negative)
             ax.imshow(img, cmap='gray')
         else:
             img = np.transpose(img, (1, 2, 0))
             if normalize:
-                img = normalize_image(img, range, False)
+                img = normalize_image(img, range, False, negative)
             ax.imshow(img)
 
-        ax.set_title(label)
+        ax.set_ylabel(label)
         ax.set_xticks([])
         ax.set_yticks([])
         # ax.axis('off')
 
 
 
-def normalize_image(img, range=(0., 1.), one_channel=False):
+def normalize_image(img, range=(0., 1.), one_channel=False, negative=False):
     """
     Linearly normalizes an image to be in range.
 
@@ -53,7 +53,10 @@ def normalize_image(img, range=(0., 1.), one_channel=False):
     """
     new_min, new_max = range
     old_min, old_max = img.min(), img.max()
-    return new_min + (img - old_min) * (new_max - new_min) / (old_max - old_min)
+    new = new_min + (img - old_min) * (new_max - new_min) / (old_max - old_min)
+    if negative: 
+        new = 1. - new
+    return new
 
 
 def group_patches(x_patch_size=8, y_patch_size=8, x_image_size=32, y_image_size=32, n_channels=3):
@@ -65,3 +68,4 @@ def group_patches(x_patch_size=8, y_patch_size=8, x_image_size=32, y_image_size=
                                                   range(x_patch_size),
                                                   range(y_patch_size))])
     return groups
+

examples/adversarial_robustness/plot_visualizing_adversarial_attacks.py
@@ -7,51 +7,65 @@ and compares the examples from different constraint sets, penalizations and solv
 
 """
 
-
-from itertools import product
-
-import numpy as np
+# TODO: REFACTOR DATASETS FROM OUR DATALOADING UTILITIES
 
 import torch
 import torchvision
-
-from robustbench.data import load_cifar10
-from robustbench.utils import load_model
+# from torchvision import transforms
+# from robustbench.data import load_cifar10
+# from robustbench.utils import load_model
 
 import matplotlib.pyplot as plt
 
 import chop
-from chop.image import group_patches, matplotlib_imshow_batch
-from chop.logging import Trace
+from chop.utils.image import group_patches, matplotlib_imshow_batch
+from chop.utils.data import ImageNet, CIFAR10, NormalizingModel
+from chop.utils.logging import Trace
 
 from sklearn.metrics import f1_score
-
-from torch.autograd import profiler
 
 # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 device = torch.device('cpu')
 
-batch_size = 8
+batch_size = 4
 
 # Note that this example uses load_cifar10 from the robustbench library
-data, target = load_cifar10(n_examples=batch_size, data_dir='~/datasets')
-data = data.to(device)
-target = target.to(device)
+# data, target = load_cifar10(n_examples=batch_size, data_dir='~/datasets')
 
-classes = ('plane', 'car', 'bird', 'cat',
-           'deer', 'dog', 'frog', 'horse',
-           'ship', 'truck')
+data_dir = "/scratch/data/imagenet12/"
+dataset = ImageNet(data_dir, normalize=False)
 
+# data_dir = "~/datasets/"
+# dataset = CIFAR10()
 
-model = load_model('Standard')  # Can be changed to any model from the robustbench model zoo
+normalize = dataset.normalize
+unnormalize = dataset.unnormalize
+
+data, target = dataset.load_k(batch_size, train=True, device=device,
+                              shuffle=True)
+classes = dataset.classes
+
+# ImageNet model
+model = torchvision.models.resnet18(pretrained=True)
+
+# CIFAR10 model
+# model = load_model('Standard')  # Can be changed to any model from the robustbench model zoo
+
 model = model.to(device)
+# Attack criterion
 criterion = torch.nn.CrossEntropyLoss(reduction='none')
+
+
+# Add first layer normalization
+# since the data is not normalized on loading.
+model = NormalizingModel(model, dataset)
 
 # Define the constraint set + initial point
 print("L2 norm constraint.")
-alpha = .5
+alpha = 3
 constraint = chop.constraints.L2Ball(alpha)
 
+max_str_length = 15 # for plot: max length of class
 
 def image_constraint_prox(delta, step_size=None):
     adv_img = torch.clamp(data + delta, 0, 1)
@@ -59,7 +73,11 @@ def image_constraint_prox(delta, step_size=None):
     return delta
 
 
+# TODO: think above using Dykstra instead of 2 alternate projections
 def prox(delta, step_size=None):
+    """This needs to clip the data renormalized to [0, 1].
+    The epsilon scale is w/ regard to this unit box constraint."""
+
     delta = constraint.prox(delta, step_size)
     delta = image_constraint_prox(delta, step_size)
     return delta
@@ -67,6 +85,8 @@ def prox(delta, step_size=None):
 
 adversary = chop.Adversary(chop.optim.minimize_pgd_madry)
 callback_L2 = Trace()
+
+# perturb's iterates are delta in [-mean / std, (1 - mean)/ std]
 _, delta = adversary.perturb(data, target, model, criterion,
                              prox=prox,
                              lmo=constraint.lmo,
@@ -75,28 +95,36 @@ _, delta = adversary.perturb(data, target, model, criterion,
                              callback=callback_L2)
 
 # Plot adversarial images
-fig, ax = plt.subplots(nrows=7, ncols=batch_size, figsize=(16, 14))
+fig, ax = plt.subplots(ncols=8, nrows=batch_size, figsize=(12, 6))
 
 # Plot clean data
-matplotlib_imshow_batch(data, labels=[classes[k] for k in target], axes=ax[0, :],
-                        title="Original images")
+matplotlib_imshow_batch(data, labels=[classes[int(k)][:max_str_length] for k in target], axes=ax[:, 0],
+                        title="Ground Truth")
+
+preds = model(data).argmax(dim=-1)
+matplotlib_imshow_batch(data, labels=[classes[int(k)][:max_str_length] for k in preds], axes=ax[:, 1],
+                        title="Prediction")
 
 # Adversarial Lp images
 adv_output = model(data + delta)
 adv_labels = torch.argmax(adv_output, dim=-1)
-matplotlib_imshow_batch(data + delta, labels=[classes[k] for k in adv_labels], axes=ax[1, :],
+matplotlib_imshow_batch(data + delta, labels=[classes[int(k)][:max_str_length] for k in adv_labels], axes=ax[:, 2],
                         title=f'L{constraint.p}')
 
 # Perturbation
-matplotlib_imshow_batch(abs(delta), axes=ax[4, :], normalize=True,
-                        title=f'L{constraint.p}')
+matplotlib_imshow_batch(abs(delta), axes=ax[:, 5], normalize=True,
+                        # title=f'L{constraint.p}', negative=True)
+                        title=f'L{constraint.p}', negative=False)
 
 
 print("GroupL1 constraint.")
 
-groups = group_patches(x_patch_size=8, y_patch_size=8)
+# CIFAR-10
+# groups = group_patches(x_patch_size=8, y_patch_size=8, x_image_size=32, y_image_size=32)
+# Imagenet
+groups = group_patches(x_patch_size=28, y_patch_size=28, x_image_size=224, y_image_size=224)
 
-for eps in [1e-2]:
+for eps in [5e-2]:
     alpha = eps * len(groups)
     constraint_group = chop.constraints.GroupL1Ball(alpha, groups)
     adversary_group = chop.Adversary(chop.optim.minimize_frank_wolfe)
@@ -104,11 +132,10 @@ for eps in [1e-2]:
     # callback_group = Trace(callable=lambda kw: criterion(model(data + kw['x']), target))
     callback_group = Trace()
 
-    with profiler.profile() as prof:
-        _, delta_group = adversary_group.perturb(data, target, model, criterion,
-                                                lmo=constraint_group.lmo,
-                                                max_iter=20,
-                                                callback=callback_group)
+    _, delta_group = adversary_group.perturb(data, target, model, criterion,
+                                            lmo=constraint_group.lmo,
+                                            max_iter=20,
+                                            callback=callback_group)
 
     
     delta_group = image_constraint_prox(delta_group)
@@ -117,19 +144,20 @@ for eps in [1e-2]:
     adv_output_group = model(data + delta_group)
     adv_labels_group = torch.argmax(adv_output_group, dim=-1)
 
-    matplotlib_imshow_batch(data + delta_group, labels=(classes[k] for k in adv_labels_group),
-                            axes=ax[2, :],
+    matplotlib_imshow_batch(data + delta_group, labels=(classes[int(k)][:max_str_length] for k in adv_labels_group),
+                            axes=ax[:, 3],
                             title='Group Lasso')
 
-    matplotlib_imshow_batch(abs(delta_group), axes=ax[5, :], normalize=True,
-                            title='Group Lasso')
+    matplotlib_imshow_batch(abs(delta_group), axes=ax[:, 6], normalize=True,
+                            # title='Group Lasso', negative=True)
+                            title='Group Lasso', negative=False)
 
     print(f"F1 score: {f1_score(target.detach().cpu(), adv_labels_group.detach().cpu(), average='macro'):.3f}"
           f" for alpha={alpha:.4f}")
 
 print("Nuclear norm ball adv examples")
 
-for alpha in [.2]:
+for alpha in [.5]:
     constraint_nuc = chop.constraints.NuclearNormBall(alpha)
 
 
@@ -156,15 +184,18 @@ for alpha in [.2]:
     adv_output_nuc = model(data + delta_nuc)
     adv_labels_nuc = torch.argmax(adv_output_nuc, dim=-1)
 
-    matplotlib_imshow_batch(data + delta_nuc, labels=(classes[k] for k in adv_labels_nuc),
-                            axes=ax[3, :],
+    matplotlib_imshow_batch(data + delta_nuc, labels=(classes[int(k)][:max_str_length] for k in adv_labels_nuc),
+                            axes=ax[:, 4],
                             title='Nuclear Norm')
 
-    matplotlib_imshow_batch(abs(delta_nuc), axes=ax[6, :], normalize=True,
-                            title='Nuclear Norm')
+    matplotlib_imshow_batch(abs(delta_nuc), axes=ax[:, 7], normalize=True,
+                            # title='Nuclear Norm', negative=True)
+                            title='Nuclear Norm', negative=False)
 
     print(f"F1 score: {f1_score(target.detach().cpu(), adv_labels_nuc.detach().cpu(), average='macro'):.3f}"
           f" for alpha={alpha:.4f}")
+
+plt.subplots_adjust(bottom=.06, top=0.5)
 
 plt.tight_layout()
 plt.show()
@@ -173,24 +204,24 @@ plt.show()
 # TODO refactor this in functions
 
 # Plot group lasso loss values
-fig, ax = plt.subplots(figsize=(6, 10), nrows=batch_size, sharex=True)
-for k in range(batch_size):
-    ax[k].plot([-trace[k] for trace in callback_group.trace_f])
-plt.tight_layout()
-plt.show()
+# fig, ax = plt.subplots(figsize=(6, 10), nrows=batch_size, sharex=True)
+# for k in range(batch_size):
+#     ax[k].plot([-trace[k] for trace in callback_group.trace_f])
+# plt.tight_layout()
+# plt.show()
 
-# Plot loss functions per datapoint
-fig, ax = plt.subplots(figsize=(6, 10), nrows=batch_size, sharex=True)
-for k in range(batch_size):
-    ax[k].plot([-trace[k] for trace in callback_nuc.trace_f])
+# # Plot loss functions per datapoint
+# fig, ax = plt.subplots(figsize=(6, 10), nrows=batch_size, sharex=True)
+# for k in range(batch_size):
+#     ax[k].plot([-trace[k] for trace in callback_nuc.trace_f])
 
-plt.tight_layout()
-plt.show()
+# plt.tight_layout()
+# plt.show()
 
-# Plot loss functions per datapoint
-fig, ax = plt.subplots(figsize=(6, 10), nrows=batch_size, sharex=True)
-for k in range(batch_size):
-    ax[k].plot([-trace[k] for trace in callback_L2.trace_f])
+# # Plot loss functions per datapoint
+# fig, ax = plt.subplots(figsize=(6, 10), nrows=batch_size, sharex=True)
+# for k in range(batch_size):
+#     ax[k].plot([-trace[k] for trace in callback_L2.trace_f])
 
-plt.tight_layout()
-plt.show()
+# plt.tight_layout()
+# plt.show()

examples/plot_optim_dynamics.py
@@ -10,7 +10,7 @@ import torch
 
 from chop.constraints import LinfBall
 from chop.optim import minimize_frank_wolfe, minimize_pgd, minimize_pgd_madry, minimize_three_split
-from chop import utils
+from chop import utils 
 
 torch.random.manual_seed(0)
 