c8b7b5183e0f78179aed46b24b9be427012eddf5
Geoffrey Negiar <geoffrey_negiar@berkeley.edu>
2021-03-03 16:44:27 -0800

    Tentative fix for utils import issue

--- chop/__init__.py
+++ chop/__init__.py
@@ -1,10 +1,10 @@
 """chop: constrained optimization for PyTorch"""
 __version__ = "0.0.1"
 
+from . import utils
 from . import stochastic
 from . import optim
 from . import constraints
 from . import penalties
-from . import utils
 from . import adversary
 from .adversary import Adversary
