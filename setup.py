e5d0ec80fc7de0b86fe61416dc4a978158e48587
Geoffrey Negiar <geoffrey_negiar@berkeley.edu>
2021-03-04 17:40:29 -0800

    Setup updates

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ CLASSIFIERS = """\
 Development Status :: 3 - Alpha
 Intended Audience :: Science/Research
 Intended Audience :: Developers
-License :: OSI Approved
+License :: OSI Approved :: BSD License
 Programming Language :: Python
 Programming Language :: Python :: 3
 Topic :: Software Development
@@ -14,10 +14,13 @@ Operating System :: POSIX
 Operating System :: Unix
 """
 
+README = io.open("README.md", encoding="utf-8").read()
+
 setup(
-    name="chop",
-    description="Library for continuous optimization using PyTorch",
-    long_description=io.open("README.md", encoding="utf-8").read(),
+    name="chop-pytorch",
+    description="Continuous and constrained optimization with PyTorch",
+    long_description=README,
+    long_description_content_type='text/markdown',
     version="0.0.2",
     author="Geoffrey Negiar",
     author_email="geoffrey_negiar@berkeley.edu",
@@ -25,5 +28,4 @@ setup(
     packages=["chop"],
     install_requires=["numpy", "scipy", "torch"],
     classifiers=[_f for _f in CLASSIFIERS.split("\n") if _f],
-    license="New BSD License",
 )
