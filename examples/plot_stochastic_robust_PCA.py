34b99a7e6acc01f8e52df134f8effd4de4d44cbf
Geoffrey Négiar <geoffrey_negiar@berkeley.edu>
2021-05-13 19:09:59 -0700

    Changed dataloader + loss for efficiency

--- examples/plot_stochastic_robust_PCA.py
+++ examples/plot_stochastic_robust_PCA.py
@@ -34,7 +34,9 @@ r_p = [(5, 1e-3),
        (25, 3e-2), (130, 1e-2)
        ]
 
-n_epochs = 400
+n_epochs = 200
+
+sqloss = torch.nn.MSELoss()
 
 for r, p in r_p:
     print(f'r={r} and p={p}')
@@ -42,21 +44,18 @@ for r, p in r_p:
     V = torch.normal(torch.zeros(r, n))
 
     # Low rank component
-    L = 10 * utils.bmm(U, V)
+    L = 10 * utils.bmm(U, V).to(device)
 
     # Sparse component
-    S = 100 * torch.normal(torch.zeros(m, n))
+    S = 100 * torch.normal(torch.zeros(m, n)).to(device)
 
     S *= (torch.rand_like(S) <= p)
 
     # Add noise
-    N = torch.normal(torch.zeros(m, n))
+    N = torch.normal(torch.zeros(m, n)).to(device)
 
     M = L + S + N
     M = M.to(device)
-
-    def sqloss(Z, M):
-        return .5 / Z.numel() * torch.linalg.norm((Z - M).squeeze(), ord='fro') ** 2
 
     rnuc = torch.linalg.norm(L.squeeze(), ord='nuc')
     sL1 = abs(S).sum()
@@ -71,20 +70,17 @@ for r, p in r_p:
     prox = sparsity_constraint.prox
     prox_lr = rank_constraint.prox
 
-    Z = torch.zeros_like(M, device=device)
-    Z.requires_grad_(True)
-
     batch_sizes = [100, 250, 500, 1000]
     fig, axes = plt.subplots(nrows=2, ncols=len(batch_sizes), figsize=(18, 10), sharey=True)
     fig.suptitle(f'r={r} and p={p}')
 
     for batch_size, ax_it, ax_time in zip(batch_sizes, axes[0], axes[1]):
+        Z = torch.zeros_like(M, device=device)
+        Z.requires_grad_(True)
         print(f"Batch size: {batch_size}")
-        sampler = torch.utils.data.BatchSampler(torch.utils.data.RandomSampler(range(M.size(0))),
-                                                batch_size=batch_size,
-                                                drop_last=True)
-
-        momentum = .9 if batch_size != 1000 else 0.
+        dataset = torch.utils.data.TensorDataset(Z, M)
+        loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
+        momentum = .9 if batch_size != m else 0.
 
         optimizer = chop.stochastic.SplittingProxFW([Z], lmo=[lmo],
                                                     prox1=[prox],
@@ -94,18 +90,19 @@ for r, p in r_p:
                                                     normalization='none',
                                                     momentum=momentum)
 
+
         train_losses = []
-        start = time()
         times = []
         losses = []
         sgrad_avg = 0
         n_it = 0
         freq = 10
+        start = time()
         for it in range(n_epochs):
-            for idx in sampler:
+            for zi, mi in loader:
                 n_it += 1
                 optimizer.zero_grad()
-                loss = sqloss(Z[idx], M[idx])
+                loss = sqloss(zi, mi)
                 loss.backward()
                 sgrad = Z.grad.detach().clone()
                 sgrad_avg += sgrad
@@ -114,9 +111,11 @@ for r, p in r_p:
                     with torch.no_grad():
                         times.append(time() - start)
                         full_loss = sqloss(Z, M)
+                        print(full_loss)
                         train_losses.append(loss.item())
                         losses.append(full_loss.item())
                 optimizer.step()
+        donetime = time()
 
         # Get sparse and LR component
         state = optimizer.state[Z]
@@ -133,8 +132,10 @@ for r, p in r_p:
         ax_time.set_xlabel('time (s)')
         ax_it.set_yscale('log')
         ax_it.legend()
-
+        print(f"Low rank loss: {torch.linalg.norm(L - lr_comp) / torch.linalg.norm(L)}")
+        print(f"Sparse loss: {torch.linalg.norm(S - sparse_comp) / torch.linalg.norm(S)}")
+        print(f"Reconstruction loss: {torch.linalg.norm(M - sparse_comp - lr_comp) / torch.linalg.norm(M)}")
+        print(f"Time: {times[-1]}s")
     fig.show()
     fig.savefig("robustPCA.png")
-    break
 print("Done.")
